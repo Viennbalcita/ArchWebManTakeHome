cf8e11bbc231b7954d2489b977f8ca8cc9e38c17
Vienn Rowen Balcita <164728398+Viennbalcita@users.noreply.github.com>
2025-09-21 12:46:29 +0800

    Updated server.mjs

--- src/server.mjs
+++ src/server.mjs
@@ -2,15 +2,10 @@ import express from 'express';
 import mongoose from 'mongoose';
 import bodyParser from 'body-parser';
 // Model for videos
-// import video from '../models/video.mjs';
+import video from './models/video.mjs';
 
-// //routes
-// import create from '../routes/create.mjs';
-// import deletevideo from '../routes/deletevideo.mjs';
-// import get from '../routes/get.mjs';
-// import getLatest from '../routes/get-latest.mjs';
-// import selectAll from '../routes/select-all.mjs';
-// import update from '../routes/update.mjs';
+// Route
+import videoRoutes from './routes/videoRoutes.mjs';
 
 const app = express();
 const PORT = 5000
@@ -23,9 +18,5 @@ mongoose.connect('mongodb://localhost:27017/ArchBackendChallenge')
     .then(() => console.log('Connected to MongoDB'))
     .catch((error) => console.error('Error connecting to MongoDB:', error));
 
-// app.use('/api/videos', create);
-// app.use('/api/videos', deletevideo);
-// app.use('/api/videos', get);
-// app.use('/api/videos', getLatest);
-// app.use('/api/videos', selectAll);
-// app.use('/api/videos', update);
+app.use('/routes/video', videoRoutes)
+
